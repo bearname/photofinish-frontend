5cb158d3805f0c2db0676de5ade35d56944c0126
bearname <ya.mikushoff@gmail.com>
2021-11-24 19:51:52 +0300

    'fix'

---
+++ src/components/icons/IconDelete.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "IconDelete"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/icons/IconNavigateNext.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "IconNavigateNext"
+}
+</script>
+
+<style scoped>
+
+</style>
